52619815daf140ac8141d178c3a5e8884d545800
Michiel De Smet <mdesmet@gmail.com>
2021-08-17 22:12:30 +0200

    dagster-dbt working asset materializations

--- dbt_pipeline/repo.py
+++ dbt_pipeline/repo.py
@@ -1,19 +1,13 @@
-from dagster import pipeline, repository, schedule, solid, ModeDefinition
-from dagster_dbt import dbt_cli_resource
+from dagster import pipeline, repository, schedule
+from dagster_dbt import dbt_cli_run, dbt_cli_seed
 
-my_dbt_resource = dbt_cli_resource.configured({"profiles_dir": "."})
+config = {"profiles-dir": "."}
+run_all_models = dbt_cli_run.configured(config, name="run_all_models")
+run_all_seeds = dbt_cli_seed.configured(config, name="run_all_seeds")
 
-@solid(required_resource_keys={"dbt"})
-def run_all_seeds(context):
-    context.resources.dbt.seed()
-
-@solid(required_resource_keys={"dbt"})
-def run_all_models(context, seeds):
-    context.resources.dbt.run()
-
-@pipeline(mode_defs=[ModeDefinition(resource_defs={"dbt": my_dbt_resource})])
+@pipeline
 def my_dbt_pipeline():
-    run_all_models(run_all_seeds())
+    run_all_models(start_after=run_all_seeds())
 
 
 @schedule(cron_schedule="0 * * * *", pipeline_name="my_dbt_pipeline", execution_timezone="US/Central")
